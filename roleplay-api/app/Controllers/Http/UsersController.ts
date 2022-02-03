98b74194b3ec2185026ec0c3d628d6d8d23d0d92
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-03 12:42:35 -0300

    feat(UsersController): Throw BadRequest if email already in use

--- roleplay-api/app/Controllers/Http/UsersController.ts
+++ roleplay-api/app/Controllers/Http/UsersController.ts
@@ -1,8 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequest from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const userByEmail = await User.findBy('email', userPayload.email)
+
+    if (userByEmail) throw new BadRequest('email already in use', 409)
+
     const user = await User.create(userPayload)
     return response.created({ user })
   }

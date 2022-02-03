7a59def9ba9f998944f8806c9b610461f2ecf4e4
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-03 12:45:41 -0300

    feat(UsersController): Throw BadRequest if username already in use

--- roleplay-api/app/Controllers/Http/UsersController.ts
+++ roleplay-api/app/Controllers/Http/UsersController.ts
@@ -4,9 +4,12 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
-    const userByEmail = await User.findBy('email', userPayload.email)
 
+    const userByEmail = await User.findBy('email', userPayload.email)
     if (userByEmail) throw new BadRequest('email already in use', 409)
+
+    const userByUsername = await User.findBy('username', userPayload.username)
+    if (userByUsername) throw new BadRequest('username already in use', 409)
 
     const user = await User.create(userPayload)
     return response.created({ user })

8e2caeb70c46d088024b4929bc8189a008498433
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-03 12:40:55 -0300

    test(UsersController): Ensure return 409 when email is already in use

--- roleplay-api/test/users/users.spec.ts
+++ roleplay-api/test/users/users.spec.ts
@@ -1,8 +1,10 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group('User', () => {
+test.group('User', (group) => {
   test('it should create an user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
@@ -17,5 +19,30 @@ test.group('User', () => {
     assert.equal(body.user.username, userPayload.username)
     assert.equal(body.user.avatar, userPayload.avatar)
     assert.notExists(body.user.password, 'Password defined')
+  })
+
+  test('it should return 409 when email is already in use', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email,
+        username: 'test',
+        password: 'test',
+      })
+      .expect(409)
+    assert.exists(body.message)
+    assert.exists(body.code)
+    assert.exists(body.status)
+    assert.include(body.message, 'email')
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
   })
 })

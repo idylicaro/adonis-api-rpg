92aac0490272ffb5edf660a69d1937474ced012d
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-09 16:26:01 -0300

    test(Session): Ensure return an api token when session is created

--- roleplay-api/test/users/sessions.spec.ts
+++ roleplay-api/test/users/sessions.spec.ts
@@ -18,6 +18,18 @@ test.group('Session', (group) => {
     assert.equal(body.user.id, id)
   })
 
+  test.only('it should return an api token when session is created', async (assert) => {
+    const plainPassword = 'test'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    assert.isDefined(body.token, 'Token undefined')
+    assert.equal(body.user.id, id)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })

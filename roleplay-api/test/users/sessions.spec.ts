f66cac27cadd10ed5b5d1c2863854109d4c9cb31
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-09 16:50:34 -0300

    test(Session): Ensure  return 400 if credentials not provided or invalid

--- roleplay-api/test/users/sessions.spec.ts
+++ roleplay-api/test/users/sessions.spec.ts
@@ -18,7 +18,7 @@ test.group('Session', (group) => {
     assert.equal(body.user.id, id)
   })
 
-  test.only('it should return an api token when session is created', async (assert) => {
+  test('it should return an api token when session is created', async (assert) => {
     const plainPassword = 'test'
     const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
     const { body } = await supertest(BASE_URL)
@@ -28,6 +28,25 @@ test.group('Session', (group) => {
 
     assert.isDefined(body.token, 'Token undefined')
     assert.equal(body.user.id, id)
+  })
+
+  test('it should return 400 when credentials are not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/sessions').send({}).expect(400)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 400)
+  })
+
+  test('it should return 400 when credentials are invalid', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email,
+        password: 'test',
+      })
+      .expect(400)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 400)
   })
 
   group.beforeEach(async () => {

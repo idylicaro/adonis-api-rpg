a528b220fd66449e319e68fb23e87ce4b001d32e
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-09 16:25:53 -0300

    feat(Session): Ensure return an api token when session is created

--- roleplay-api/app/Controllers/Http/SessionsController.ts
+++ roleplay-api/app/Controllers/Http/SessionsController.ts
@@ -3,7 +3,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
-    await auth.attempt(email, password)
-    response.created({ user: auth.user })
+    const token = await auth.use('api').attempt(email, password, { expiresIn: '2hours' })
+    response.created({ user: auth.user, token })
   }
 }

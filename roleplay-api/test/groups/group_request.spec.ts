629da8aded95144176ec285aabcc272a734a724c
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-10 14:42:52 -0300

    test(GroupRequest): Ensure return 409 and 422 if already exists or member is already in the group

--- roleplay-api/test/groups/group_request.spec.ts
+++ roleplay-api/test/groups/group_request.spec.ts
@@ -22,6 +22,49 @@ test.group('Group Request', (group) => {
     assert.equal(body.groupRequest.status, 'PENDING')
   })
 
+  test('it should return 409 when group request already exists', async (assert) => {
+    const { id } = await UserFactory.create() //master
+    const group = await GroupFactory.merge({ master: id }).create()
+    await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${apiToken}`)
+      .send({})
+      .expect(201)
+
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id,
+    }
+  })
+
+  test('it should return 422 when user is already in the group', async (assert) => {
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id,
+    }
+    const response = await supertest(BASE_URL)
+      .post(`/groups`)
+      .set('Authorization', `Bearer ${apiToken}`)
+      .send(groupPayload)
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${response.body.group.id}/requests`)
+      .set('Authorization', `Bearer ${apiToken}`)
+      .send({})
+      .expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
   group.before(async () => {
     const plainPassword = 'test'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()

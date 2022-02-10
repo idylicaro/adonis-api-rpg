48d6c8bbe38132aecdb84b4369a2728fe0635776
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-10 16:08:16 -0300

    test(GroupRequest.Index): Ensure lists group requests by master

--- roleplay-api/test/groups/group_request.spec.ts
+++ roleplay-api/test/groups/group_request.spec.ts
@@ -65,6 +65,59 @@ test.group('Group Request', (group) => {
     assert.equal(body.status, 422)
   })
 
+  test('it should list group requests by master', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${apiToken}`)
+      .send({})
+
+    const groupRequest = response.body.groupRequest
+
+    const { body } = await supertest(BASE_URL)
+      .get(`/groups/${group.id}/requests?master=${master.id}`)
+      .expect(200)
+
+    assert.exists(body.groupRequests, 'GroupRequests undefined')
+    assert.equal(body.groupRequests.length, 1)
+    assert.equal(body.groupRequests[0].id, groupRequest.id)
+    assert.equal(body.groupRequests[0].userId, groupRequest.userId)
+    assert.equal(body.groupRequests[0].groupId, groupRequest.groupId)
+    assert.equal(body.groupRequests[0].status, groupRequest.status)
+    assert.equal(body.groupRequests[0].group.name, group.name)
+    assert.equal(body.groupRequests[0].user.username, user.username)
+    assert.equal(body.groupRequests[0].group.master, master.id)
+  })
+
+  test('it should return an empty list when master has no group requests', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${apiToken}`)
+      .send({})
+
+    const { body } = await supertest(BASE_URL)
+      .get(`/groups/${group.id}/requests?master=${user.id}`)
+      .expect(200)
+
+    assert.exists(body.groupRequests, 'GroupRequests undefined')
+    assert.equal(body.groupRequests.length, 0)
+  })
+
+  test('it should return 422 when master is not provided', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL).get(`/groups/${group.id}/requests`).expect(422)
+
+    assert.exists(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
   group.before(async () => {
     const plainPassword = 'test'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()

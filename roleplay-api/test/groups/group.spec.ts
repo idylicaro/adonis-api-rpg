011a1b8ac6a9acabfbf0162d7a8335a04bdb58b1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-10 07:06:58 -0300

    test(Group): Ensure return 422 if data is not provided or invalid

--- roleplay-api/test/groups/group.spec.ts
+++ roleplay-api/test/groups/group.spec.ts
@@ -6,7 +6,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Group', (group) => {
-  test.only('it should create a group', async (assert) => {
+  test('it should create a group', async (assert) => {
     const user = await UserFactory.create()
     const groupPayload = {
       name: 'test',
@@ -24,6 +24,11 @@ test.group('Group', (group) => {
     assert.equal(body.group.location, groupPayload.location)
     assert.equal(body.group.chronic, groupPayload.chronic)
     assert.equal(body.group.master, groupPayload.master)
+  })
+  test('it should return 422 when required data is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/groups').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
   })
 
   group.beforeEach(async () => {

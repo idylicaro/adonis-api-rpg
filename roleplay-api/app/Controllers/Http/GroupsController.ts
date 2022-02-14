cfd3c7881b0f8ff583c64d67b351b1d0375f8e7b
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-02-14 10:42:32 -0300

    refactor(Group): Use Query Scope and remove duplicated query filters

--- roleplay-api/app/Controllers/Http/GroupsController.ts
+++ roleplay-api/app/Controllers/Http/GroupsController.ts
@@ -11,46 +11,6 @@ export default class GroupsController {
 
     response.ok({ groups })
   }
-
-  private filterByQueryString(userId: number, text: string) {
-    if (userId && text) return this.filterByUserAndText(userId, text)
-    else if (userId) return this.filterByUser(userId)
-    else if (text) return this.filterByText(text)
-    else return this.all()
-  }
-
-  private all() {
-    return Group.query().preload('players').preload('masterUser')
-  }
-
-  private filterByUser(userId: number) {
-    return Group.query()
-      .preload('players')
-      .preload('masterUser')
-      .whereHas('players', (query) => {
-        query.where('id', userId)
-      })
-  }
-
-  private filterByText(text: string) {
-    return Group.query()
-      .preload('players')
-      .preload('masterUser')
-      .where('name', 'LIKE', `%${text}%`)
-      .orWhere('description', 'LIKE', `%${text}%`)
-  }
-
-  private filterByUserAndText(userId: number, text: string) {
-    return Group.query()
-      .preload('players')
-      .preload('masterUser')
-      .whereHas('players', (query) => {
-        query.where('id', userId)
-      })
-      .where('name', 'LIKE', `%${text}%`)
-      .orWhere('description', 'LIKE', `%${text}%`)
-  }
-
   public async store({ request, response }: HttpContextContract) {
     const groupPayload = await request.validate(CreateGroup)
     const group = await Group.create(groupPayload)
@@ -93,5 +53,38 @@ export default class GroupsController {
 
     await group.delete()
     return response.ok({})
+  }
+
+  private filterByQueryString(userId: number, text: string) {
+    if (userId && text) return this.filterByUserAndText(userId, text)
+    else if (userId) return this.filterByUser(userId)
+    else if (text) return this.filterByText(text)
+    else return this.all()
+  }
+
+  private all() {
+    return Group.query().preload('players').preload('masterUser')
+  }
+
+  private filterByUser(userId: number) {
+    return Group.query()
+      .preload('players')
+      .preload('masterUser')
+      .withScopes((scope) => scope.withPlayer(userId))
+  }
+
+  private filterByText(text: string) {
+    return Group.query()
+      .preload('players')
+      .preload('masterUser')
+      .withScopes((scope) => scope.withText(text))
+  }
+
+  private filterByUserAndText(userId: number, text: string) {
+    return Group.query()
+      .preload('players')
+      .preload('masterUser')
+      .withScopes((scope) => scope.withPlayer(userId))
+      .withScopes((scope) => scope.withText(text))
   }
 }
